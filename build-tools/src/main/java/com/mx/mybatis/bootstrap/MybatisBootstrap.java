bcd6d3b52fdee9159384252ff9a86f8bd1105c72
zhangdong <lhachidog@sina.com>
2023-11-28 17:39:46 +0800

    mapperScan

--- build-tools/src/main/java/com/mx/mybatis/bootstrap/MybatisBootstrap.java
+++ build-tools/src/main/java/com/mx/mybatis/bootstrap/MybatisBootstrap.java
@@ -1,11 +1,9 @@
 package com.mx.mybatis.bootstrap;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan
 public class MybatisBootstrap {
 
 	public static void main(String[] args) {
